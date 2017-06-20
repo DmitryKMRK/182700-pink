4bf63a91241370d847517c792d403841bba2cbe8
DmitryKMRK <dmitry.kmrk@gmail.com>
2017-06-20 08:55:40 +0300

    Upgrade

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+var toggle = document.querySelector('.header__toggle');
+var header = document.querySelector('.header__top');
+var menu = document.querySelector('.header__nav');
+var state = 'close';
+
+menu.classList.remove('header__nav--open');
+toggle.classList.remove('header__toggle--nojs');
+header.classList.remove('header__top--open');
+
+toggle.addEventListener('click', function() {
+  if(state === 'close') {
+    header.classList.add('header__top--open');
+    menu.classList.add('header__nav--open');
+    toggle.classList.add('header__toggle--open');
+    state = 'open';
+  } else {
+    header.classList.remove('header__top--open');
+    menu.classList.remove('header__nav--open');
+    toggle.classList.remove('header__toggle--open');
+    state = 'close';
+  }
+});
+
+/*
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -12,3 +36,4 @@ navToggle.addEventListener('click', function() {
       navMain.classList.remove('main-nav--opened');
   }
 });
+*/
